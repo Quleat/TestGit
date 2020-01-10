7a9610ed69bc9ddde03228ac61b514357927e07e
Quleat <Kintoxo86@gmail.com>
2020-01-10 09:36:10 +0300

    Main unity

---
+++ Contra/Assets/Scripts/CharacterControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControl : MonoBehaviour
+{
+    public Rigidbody2D Character;
+    public float inputx;
+    public float speed = 50f;
+    public float thrust = 100f;
+    Check check;
+    void Start()
+    {
+        Character = GetComponent<Rigidbody2D>();
+        //check = FindObjectOfType
+    }
+   void Update()
+    {
+        inputx = Input.GetAxis("Horizontal");
+        Vector2 movement = new Vector2(inputx, Character.velocity.y) * Time.deltaTime * speed;
+        Character.velocity = movement;
+        if (Input.GetKeyDown(KeyCode.W)) Character.AddForce(Vector2.up * thrust, ForceMode2D.Force);
+    }
+}
