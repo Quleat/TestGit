903121d66ff932d547505532f4f812656ea668c1
Quleat <Kintoxo86@gmail.com>
2020-01-14 18:16:15 +0300

    2

--- Contra2D/Assets/Scripts/CharacterControl.cs
+++ Contra2D/Assets/Scripts/CharacterControl.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 public class CharacterControl : MonoBehaviour
 {
     public Rigidbody2D Character;
+    public Rigidbody2D bp;
     public float inputx;
     public float speed = 50f;
     public float thrust = 100f;
     Check check;
+    public GameObject Bullet;
+    public Transform player;
     void Start()
     {
         Character = GetComponent<Rigidbody2D>();
+        
         //check = FindObjectOfType
     }
    void Update()
@@ -20,5 +24,12 @@ public class CharacterControl : MonoBehaviour
         Vector2 movement = new Vector2(inputx, Character.velocity.y) * Time.deltaTime * speed;
         Character.velocity = movement;
         if (Input.GetKeyDown(KeyCode.W)) Character.AddForce(new Vector2(0,600));
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            GameObject Bulletprefab = Instantiate(Bullet) as GameObject;
+            Bulletprefab.transform.position = player.position;
+            bp = Bulletprefab.GetComponent<Rigidbody2D>();
+            bp.AddForce(new Vector2(20, 0) * Time.deltaTime);
+        }
     }
 }
