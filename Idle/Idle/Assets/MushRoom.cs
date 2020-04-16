5f652fe951c2d074de5a04dde80297813578857f
Quleat <Kintoxo86@gmail.com>
2020-04-17 00:08:30 +0300

    idle

---
+++ Idle/Idle/Assets/MushRoom.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MushRoom : MonoBehaviour
+{
+    Rigidbody2D rb;
+    int direction = 1;
+    public LayerMask WhatIsWall;
+    public bool dig = false;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void FixedUpdate()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.TransformDirection(Vector2.right), 0.1f, WhatIsWall);
+        if (hit && !dig)
+        {
+            if (hit.collider.gameObject.tag == "1st")
+            {
+                Score.AddPoints();
+                transform.Rotate(new Vector3(0, 180, 0), Space.Self);
+            }
+            else
+            {
+                StartCoroutine(Diging());
+            }
+        }
+        else
+        {
+            rb.velocity = transform.TransformDirection(Vector2.right);
+        }
+        Debug.DrawRay(transform.position, transform.TransformDirection(Vector2.right));
+    }
+    IEnumerator Diging()
+    {
+        dig = true;
+        rb.velocity = new Vector2(0, 0);
+        yield return new WaitForSeconds(5f);
+        transform.Rotate(new Vector3(0, 180, 0), Space.Self);
+        dig = false;
+    }
+}
---
+++ Idle/Idle/Assets/Score.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public static int points = 0;
+    public static Text _text;
+    public Text text;
+    public static int amount = 10;
+
+    void Start()
+    {
+        _text = text;
+    }
+    public static void AddPoints()
+    {
+        points += amount;
+        _text.text = points.ToString();
+    }
+    public void IncreaseAmount()
+    {
+        if (points >= (amount * 3)) 
+        {
+            points -= amount * 3;
+            amount *= 2;
+            _text.text = points.ToString();
+        }
+    }
+}
