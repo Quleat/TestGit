712edc8b0792fc98c8ceab8de37acb66a9b9bad1
Quleat <Kintoxo86@gmail.com>
2020-04-17 18:34:38 +0300

    idleV2.1

--- Idle/Idle/Assets/Scripts/Score.cs
+++ Idle/Idle/Assets/Scripts/Score.cs
@@ -5,20 +5,25 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class Score : MonoBehaviour
-{
-    public static int points = 0;
+{ 
     public static Text _text;
     public static Slider _slider;
     public Text text;
     public Slider slider;
     public static int amount = 10;
 
+    private bool maxMushrooms = false;
+
+    public GameObject platformPrefab;
+    public GameObject mushroomPrefab;
+
     void Start()
     {
-        text = 
+        Canvas canvas = GetComponent<Canvas>();
+        canvas.worldCamera = Camera.main;
         _text = text;
         _slider = slider;
-        DontDestroyOnLoad(this);
+        DontDestroyOnLoad(gameObject);
     }
     public static void AddPoints()
     {
@@ -40,5 +45,18 @@ public class Score : MonoBehaviour
         gameData.points %= 10;
         _text.text = gameData.points.ToString();
         if (slider.value >= slider.maxValue) SceneManager.LoadScene(1);
+    }
+    public void AddNewOne()
+    {
+        if (gameData.points >= 20)
+        {
+            GameObject _platform = Instantiate(platformPrefab) as GameObject;
+            _platform.transform.position = new Vector3(-3.7f, -0.48f, 0);
+            GameObject _mushroom = Instantiate(mushroomPrefab) as GameObject;
+            _mushroom.transform.position = new Vector3(-4f, -0.28f, 0);
+            gameData.points -= 20;
+            maxMushrooms = true;
+            _text.text = gameData.points.ToString();
+        }
     }
 }
