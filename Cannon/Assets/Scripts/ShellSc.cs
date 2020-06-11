8931f8d302581e7e2784c86512d486a21b0cc55d
Quleat <Kintoxo86@gmail.com>
2020-06-11 19:18:30 +0300

    Cannon

--- Cannon/Assets/Scripts/ShellSc.cs
+++ Cannon/Assets/Scripts/ShellSc.cs
@@ -5,8 +5,50 @@ using UnityEngine;
 public class ShellSc : MonoBehaviour
 {
     public float distance;
-    public void activate(float distance)
+    public float x;
+    public Vector2 speed = new Vector2(1,1);
+    Rigidbody2D rb;
+    public float firYforce;
+    public float curYForce = 1f;
+    public float minYForce = 1f;
+    public float xForce = 1f;
+    public float interpolation = 0.5f;
+    void Awake()
     {
-       
+        rb = GetComponent<Rigidbody2D>();
+    }
+    public void activate(float distance, float x)
+    {
+        this.distance = distance;
+        this.x = x;
+        rb.velocity = new Vector2(1,0);
+        Debug.Log(distance);
+       StartCoroutine(onAir());
+    }
+    public IEnumerator onAir()
+    {
+        curYForce = (distance / 2) / xForce;
+        minYForce = -curYForce;
+        while(true){
+        if(transform.position.x < x - (distance / 2))
+        {
+            curYForce = Mathf.Lerp(curYForce, 0,  interpolation);
+            rb.velocity = new Vector2(xForce,curYForce);
+        }
+        else if(transform.position.x > x - (distance / 2))
+        {
+            curYForce *= 2; 
+            rb.velocity = new Vector2(xForce, -curYForce);
+        }
+        else
+        {
+            rb.velocity = new Vector2(1, 0);
+        }
+        yield return new WaitForSeconds(curYForce > 0 ? curYForce : Mathf.Abs(minYForce - curYForce));
+        }
+    }
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
     }
 }
--- Cannon/Assets/Scripts/Volley.cs
+++ Cannon/Assets/Scripts/Volley.cs
@@ -5,13 +5,19 @@ using UnityEngine.UI;
 
 public class Volley : MonoBehaviour
 {
-   public GameObject shell;
-
+  public GameObject shell;
+  public void Update()
+  {
+    if(Input.GetMouseButtonDown(0))
+    {
+      Launch();
+    }
+  }
   public void Launch()
   {
-    float distance =  Camera.main.ScreenToWorldPoint(Input.mousePosition).x;
-    Debug.Log(distance);
+    float distance =  Mathf.Abs(Camera.main.ScreenToWorldPoint(Input.mousePosition).x - transform.position.x);
+    //Debug.Log(distance);
     GameObject missle = Instantiate(shell, transform.position, Quaternion.Euler(0,0,0)) as GameObject;
-    missle.GetComponent<Rigidbody2D>().velocity = new Vector2(Mathf.Abs(transform.position.x - distance), 5f);
+    missle.GetComponent<ShellSc>().activate(distance, Camera.main.ScreenToWorldPoint(Input.mousePosition).x);
   }
 }
